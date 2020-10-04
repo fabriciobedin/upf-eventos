63cbe1e5aac8c706a185e45ed2f51182bf77dd9e
Alexandre Reveilleau dos Reis <alexandrerreis@gmail.com>
2020-10-04 08:56:32 -0300

    ajustado import

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,35 +3,40 @@ import { List, ListItem, ListItemText } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 
 import { Container } from './styles';
-consulimport {Group, Home, Event, EventNoteOutlined, Person} from '@material-ui/icons';
-
+import {
+  Group,
+  Home,
+  Event,
+  EventNoteOutlined,
+  Person
+} from '@material-ui/icons';
 
 function Sidebar() {
-    const items = [
-      { name: '/app', label: 'Home', Icon: Home },
-      { name: '/eventos', label: 'Eventos', Icon: Event },
-      { name: '/participantes', label: 'Participantes', Icon: Group },
-      { name: '/subevento', label: 'Subeventos', Icon: EventNoteOutlined },
-      { name: '/usuarios', label: 'Usuários', Icon: Person }
-    ];
-    return (
-        <Container>
-            <List disablePadding dense>
-                {items.map(({ label, name, Icon, ...rest }) => (
-                    <ListItem
-                        component={RouterLink}
-                        key={name}
-                        button
-                        {...rest}
-                        to={name}
-                    >
-                        <Icon fontSize="small" style={{ paddingRight: 5 }} />
-                        <ListItemText>{label}</ListItemText>
-                    </ListItem>
-                ))}
-            </List>
-        </Container>
-    );
+  const items = [
+    { name: '/app', label: 'Home', Icon: Home },
+    { name: '/eventos', label: 'Eventos', Icon: Event },
+    { name: '/participantes', label: 'Participantes', Icon: Group },
+    { name: '/subevento', label: 'Subeventos', Icon: EventNoteOutlined },
+    { name: '/usuarios', label: 'Usuários', Icon: Person }
+  ];
+  return (
+    <Container>
+      <List disablePadding dense>
+        {items.map(({ label, name, Icon, ...rest }) => (
+          <ListItem
+            component={RouterLink}
+            key={name}
+            button
+            {...rest}
+            to={name}
+          >
+            <Icon fontSize="small" style={{ paddingRight: 5 }} />
+            <ListItemText>{label}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </Container>
+  );
 }
 
 export default Sidebar;
